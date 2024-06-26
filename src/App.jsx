a723ce5fc6a7867809a3f54273ac4ac690b4f4bc
Romesh Lankathilaka <romeshl@missionreadyhq.com>
2024-06-26 13:28:36 +1200

    Fixed the isLoading status not showing error by adding isValidating to the condition.

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,45 @@
 import React, { useState } from "react";
 import CustomVision from "./Components/CustomVision";
 
+
 function App() {
   const [file, setFile] = useState();
-  const [image, setImage] = useState();
+  const [image, setImage, imageRef] = useState();
 
-  function handleChange(e) {
+  // const [previousFile, setPreviousFile] = useState();
+  // const [previousImage, setPreviousImage] = useState();
+
+
+  const handleChange = (e) => {
+
     if (e.target.files.length > 0) {
       setImage(e.target.files[0]);
+
       setFile(URL.createObjectURL(e.target.files[0]));
     }
-
+    // else {
+    //   setImage(previousImage);
+    //   setFile(previousFile);
+    // }
   }
 
-  function handleClick(e) {
-    setImage();
-    setFile(null);
-  }
+  // function handleClick(e) {
+  //   setPreviousFile(file);
+  //   setPreviousImage(image);
+  //   setImage(null);
+  //   setFile(null);
+  // }
+
+
 
   return (
     <div className="font-mono w-[80%] max-w-[1200px] border-2 mx-[auto] rounded-3xl bg-blue-50 border-blue-900 mt-[100px] pt-[50px] p-[30px] gap-xl">
       <h1 className="text-blue-800 font-bold text-xl text-center underline mb-[30px]">Turner Car Insurance - Vehicle Type Detector Prototype</h1>
       <h2 className="font-bold">Add Image:</h2>
-      <input className="text-blue-700 mb-[30px]" type="file" onChange={handleChange} onClick={handleClick} />
+      <input className="text-blue-700 mb-[30px]" type="file" onChange={handleChange}  />
       {image && <img className="w-[80%] mx-auto border-2  border-blue-300 rounded-xl mb-[50px]" src={file} />}
-      
-      {image && <CustomVision image={image} /> }
+
+      {image && <CustomVision image={image} />}
     </div>
   );
 }
--- src/Components/CustomVision.jsx
+++ src/Components/CustomVision.jsx
@@ -1,58 +1,43 @@
-import useSWR, { mutate } from 'swr';
-import { useEffect, useState } from "react";
+import useSWR from 'swr';
 
-export default function CustomVision({ image }) {
+export default function CustomVision({ image  }) {
     const customVisionURL = "https://southcentralus.api.cognitive.microsoft.com/customvision/v3.0/Prediction/c40f86f9-c0e6-46b5-9535-e3f9c3b0d6ab/classify/iterations/TurnerCars/image";
 
-    const [showData, setShowData] = useState(false);
-
-    useEffect(() => {
-        setShowData(false);
-    }, [image]);
+    const { data, error, isLoading, isValidating } = useSWR(customVisionURL, fetcher);
 
     async function fetcher(url) {
-        if (image) {
-            const response = await fetch(url, {
-                method: "POST",
-                headers: {
-                    "Prediction-Key": "ebd9430dfce643debe391e0384855ebd",
-                    "Content-Type": "application/octet-stream",
-                },
-                body: image
-            });
-            const data = await response.json();
-            setShowData(true);
-            const probability = await Object.entries(data)[4][1][0].probability;
-            const vehicle = await Object.entries(data)[4][1][0].tagName.toUpperCase();
-            if (probability > 0.45) {
-                return "Vehicle type is: " + vehicle;
-            }
-            else {
-                return "Unable to detect the type of vehicle. Please try a different image.";
-            }
+        console.log("Component");
+        console.log(image);
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Prediction-Key": "ebd9430dfce643debe391e0384855ebd",
+                "Content-Type": "application/octet-stream",
+            },
+            body: image
+        });
+        const data = await response.json();
+        const probability = await Object.entries(data)[4][1][0].probability;
+        const vehicle = await Object.entries(data)[4][1][0].tagName.toUpperCase();
+        if (probability > 0.45) {
+            return "Vehicle type is: " + vehicle;
         }
         else {
-            return "Please upload an image first.";
+            return "Unable to detect the type of vehicle. Please try a different image.";
         }
-
     }
-
-    const { data, error, isLoading } = useSWR(customVisionURL, fetcher);
 
     return (
         <>
             <div className="w-[80%] mx-auto border-2 text-center text-xl bg-blue-100 p-4 border-blue-300 rounded-xl" >
-                {isLoading ?
+                {isLoading || isValidating ?
                     <h1>Loading data ...</h1>
                     :
                     error ?
                         <h1>Error occurred while loading data.</h1>
                         :
                         data ?
-                            <div>
-
-                                <p>{showData ? data : "Loading data ..."}</p>
-                            </div>
+                            <p>{data}</p>
                             :
                             <h1>No data to display.</h1>
                 }
